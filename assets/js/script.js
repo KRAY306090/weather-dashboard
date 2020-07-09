1d393618f88cde5e0bf14ae3b2489bdf2b755d8c
Kennadee <kennadee.ray@gmail.com>
2020-07-09 16:26:04 -0500

    api fetch added and displays city name and temp dynamically

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,21 @@
+var searchBoxEl = document.querySelector("#searchBar");
+var cityNameEl = document.querySelector("#city");
+var tempEl = document.querySelector("#temp");
+
+var getWeather = function() {
+    var city = searchBoxEl.value.trim();
+    fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a6f5e2eac18b62704a90a174201285a8`
+    )
+    .then(response => response.json())
+    .then(data => {
+        var nameVal = data["name"]
+        var tempVal = data["main"]["temp"]
+
+        cityNameEl.innerHTML = nameVal;
+        tempEl.innerHTML = tempVal;
+    })
+    .catch(err => alert("City not found"))
+      
+};
 
