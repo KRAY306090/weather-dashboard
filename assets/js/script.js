6213a17af8cb953b545df1805c987f52a106a6b9
Kennadee <kennadee.ray@gmail.com>
2020-07-12 21:05:36 -0500

    restuctured forecast cards to be created dynamically

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,8 @@
 var searchBoxEl = document.querySelector("#searchBar");
-var cityNameEl = document.querySelector("#city");
-var tempEl = document.querySelector("#temp");
-var humidityEl = document.querySelector("#humidity");
-var windEl = document.querySelector("#wind");
 var now = moment().format("(MM/DD/YYYY)");
 var forecastContainerEl = document.querySelector("#forecast");
 var currentWeatherEl = document.querySelector("#todayWeather");
 var getUV = function(lat, long) {
-    
     fetch(
         `https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${long}&appid=a6f5e2eac18b62704a90a174201285a8`
     )
@@ -18,7 +13,6 @@ var getUV = function(lat, long) {
         console.log(index);
     })
 };
-
 var getForecast = function (city) {
     fetch(
         `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=a6f5e2eac18b62704a90a174201285a8`
@@ -26,39 +20,39 @@ var getForecast = function (city) {
         .then(response => response.json())
         .then(data => {
             console.log(data)
-            
-
             for (var i = 0; i < data.list.length; i++) {
-
                 if (data.list[i].dt_txt.indexOf("15:00:00") !== -1 ) {
-
-                    //console.log(data.list[i]);
-                    //var date = data["list"][i]["dt_txt"];
+                    //sets value of card elements
                     var date = new Date(data.list[i].dt_txt).toLocaleDateString();
-                    var description = data["list"][i]["weather"]["0"]["description"];
                     var temp = data["list"][i]["main"]["temp"];
                     var humidity = data["list"][i]["main"]["humidity"];
                     var icon = data["list"][i]["weather"]["0"]["icon"];
-
+                    //creates card to hold forecast data
                     var newCard = document.createElement("div");
                     newCard.setAttribute("class", "card bg-primary text-white");
-                    newCard.innerHTML = "<div class='card-body'><h5 class='card-title'>" + date + "</h5><img src='http://openweathermap.org/img/w/'" + icon + "'.png'/><p class='card-text'>Temp: " + temp + "</p><p class='card-text'>Humidity: " + humidity + "%</p></div>";
                     forecastContainerEl.appendChild(newCard);
-                    
-                    
+                    //creates elements and sets inner html to hold forecast data
+                    var dateEl = document.createElement("h3");
+                    dateEl.innerHTML = date;
+                    var iconEl = document.createElement("img");
+                    iconEl.setAttribute("src", "http://openweathermap.org/img/w/" + icon + ".png");
+                    iconEl.setAttribute("width", "50px");
+                    var tempEl =document.createElement("p");
+                    tempEl.innerHTML = "Temp: " + temp + "\u00B0";
+                    var humidityEl = document.createElement("p");
+                    humidityEl.innerHTML = "Humidity: " + humidity + "%";
+                    // appends elements cards
+                    newCard.appendChild(dateEl);
+                    newCard.appendChild(iconEl);
+                    newCard.appendChild(tempEl);
+                    newCard.appendChild(humidityEl); 
                 }
-
             }
             var lat = data["city"]["coord"]["lat"];
             var long = data["city"]["coord"]["lon"];
-            getUV(lat,long);
-                    
-        })
-       
-        
-        
+            getUV(lat,long);            
+        }) 
 };
-
 var getWeather = function () {
 
     var city = searchBoxEl.value.trim();
@@ -67,20 +61,18 @@ var getWeather = function () {
     )
         .then(response => response.json())
         .then(data => {
-            console.log(data)
+            console.log(data);
+            //variables that hold the values of the current weather data
             var nameVal = data["name"]
             var tempVal = data["main"]["temp"]
             var humidityVal = data["main"]["humidity"]
             var windVal = data["wind"]["speed"]
             var iconVal = data["weather"]["0"]["icon"]
-            //var iconImg = document.createElement("img");
-            //iconImg.setAttribute("src", "http://openweathermap.org/img/w/03n.png" )
-            //iconVal.setAttribute("src", "http://openweathermap.org/img/w/" + data.[0].icon + ".png" )
-            //var UV = data[]
             var weatherCard = document.createElement("div");
+            //creates card to hold current weather data
             weatherCard.setAttribute("class", "card mx-auto");
             currentWeatherEl.appendChild(weatherCard);
-
+            //creates elements and sets inner HTML text and values for weather data
             var cityName =document.createElement("h2");
             cityName.innerHTML = nameVal + " " + now;
             var weatherIcon = document.createElement("img");
@@ -92,22 +84,12 @@ var getWeather = function () {
             humid.innerHTML = "Humidity: " + humidityVal + "%";
             var windSpeed = document.createElement("p");
             windSpeed.innerHTML = "Wind Speed: " + windVal + " mph";
-            
+            //appends the created elements
             weatherCard.appendChild(cityName);
             weatherCard.appendChild(weatherIcon);
             weatherCard.appendChild(temperature);
             weatherCard.appendChild(humid);
             weatherCard.appendChild(windSpeed);
-
-            
-
-
-            /*cityNameEl.innerHTML = nameVal + " " + now; //+ " " + iconImg;
-            tempEl.innerHTML = "Temperature: " + tempVal + "\u00B0";
-            humidityEl.innerHTML = "Humidity: " + humidityVal + "%";
-            windEl.innerHTML = "Wind Speed: " + windVal + "mph";
-            */
-            //document.querySelector("#card").style.display = "block";
         })
         .catch(err => alert("City not found"));
     getForecast(city);
